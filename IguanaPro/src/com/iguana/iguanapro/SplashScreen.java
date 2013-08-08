28853be97b4034713368129b805dc97bcc806fa5
Daniel Paschall <dpaschalll@gmail.com>
2013-08-07 21:22:45 -0700

    Update to images and settings activity

--- IguanaPro/src/com/iguana/iguanapro/SplashScreen.java
+++ IguanaPro/src/com/iguana/iguanapro/SplashScreen.java
@@ -7,6 +7,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class SplashScreen extends Activity {
 
@@ -38,6 +39,18 @@ public class SplashScreen extends Activity {
 		};
 
 		timer.start();
+		
+	    // Get the message from the intent
+	    Intent intent = getIntent();
+	    String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+	    // Create the text view
+	    TextView textView = new TextView(this);
+	    textView.setTextSize(50);
+	    textView.setText(message);
+
+	    // Set the text view as the activity layout
+	    setContentView(textView);
 
 	}
 	
@@ -50,5 +63,6 @@ public class SplashScreen extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+    
 
 }
